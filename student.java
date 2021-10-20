9d93379fcf90ce35ad872acd656b6fec47b22c8a
Priya-bh <priyajha912852@gmail.com>
2021-10-20 19:11:03 +0530

    student java file

---
+++ student.java
@@ -0,0 +1,18 @@
+class Student{
+   string name;
+   int admission id;
+   float fee;
+ 
+  public static void main(String args[]){
+     Student s1=new Student();
+     s1.name="Priya Bharti";
+     s1.admission id=155;
+     s1.fee=45.200;
+   
+   System.out.println(s1.name);
+ 
+   System.out.println(s1.id);
+   System.out.println(s1.fee);
+ 
+  }
+}
