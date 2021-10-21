f65ca79ba3c642b0af5089536dbb8a29ece3e261
Priya-bh <priyajha912852@gmail.com>
2021-10-21 09:14:32 +0530

    student file

---
+++ studentmain.java
@@ -0,0 +1,35 @@
+class StudentMain{
+  public static void main(String args[]){
+  System.out.println("student name is:");
+  String str="Priya Bharti";
+  int adm=45;
+  String gen="Female";
+  String bran="CSE";
+  byte sem=5;
+  byte year=3;
+  byte rol=55;
+  char sec='A';
+  long mob=7256886243l;
+  String res="Hostler";
+  String add="Samastipur Bihar";
+  float totalfee=4.0f;
+  double semfee=42.300d;
+ System.out.println(str);
+ System.out.println(adm);
+System.out.println(gen);
+System.out.println(bran);
+System.out.println(sem);
+System.out.println(year);
+System.out.println(rol);
+System.out.println(sec);
+System.out.println(mob);
+System.out.println(res);
+System.out.println(add);
+System.out.println(totalfee);
+System.out.println(semfee);
+  
+
+  
+  
+ }
+}
