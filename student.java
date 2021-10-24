c5bf8a183da8a94178386181a50504e6822a9a2e
Priya-bh <priyajha912852@gmail.com>
2021-10-24 15:19:49 +0530

    my student file

--- student.java
+++ student.java
@@ -1,18 +1,16 @@
-class Student{
-   string name;
-   int admission id;
-   float fee;
- 
-  public static void main(String args[]){
-     Student s1=new Student();
-     s1.name="Priya Bharti";
-     s1.admission id=155;
-     s1.fee=45.200;
-   
-   System.out.println(s1.name);
- 
-   System.out.println(s1.id);
-   System.out.println(s1.fee);
- 
-  }
+
+public class Student{
+    String stuName;
+	char gen;
+	String adds;
+	float fee;
+	public String displayName(){
+	String name="Priya";
+	return name;
+	}
+	public static void main(String ar[]){
+  Student student= new Student();
+	String myName= student.displayName();
+    System.out.println("my name is :"+ myName);
+	}
 }
